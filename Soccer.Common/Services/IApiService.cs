640d73f45c32c09193c6f094e5c20b6e294d9c25
CAROLINA LOPERA LOPERA <carolinalopera162798@correo.itm.edu.co>
2020-03-08 17:25:38 -0500

    Show Torunaments on app

---
+++ Soccer.Common/Services/IApiService.cs
@@ -0,0 +1,12 @@
+﻿
+using Soccer.Common.Models;
+using System.Threading.Tasks;
+
+namespace Soccer.Common.Services
+{
+    public interface IApiService
+    {
+        Task<Response> GetListAsync<T>(string urlBase, string servicePrefix, string controller);
+    }
+}
+
